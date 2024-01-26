1ce4ab5224e318aa4f89ac8b19150c5be224817c
pew1-hue <pblabitoria@gmail.com>
2024-01-26 19:53:26 +0900

    Continue

--- engines/powerball5.js
+++ engines/powerball5.js
@@ -1,76 +1,69 @@
 const moment = require('moment-timezone')
-const game = 'powerball5'
 const interval = 5
-let tempTime = 4
-let tempSec = 30
-let miniGames = []
-let gameInfo = {}
-let adjustedSec = 35
+let game
+let timerCountDown
+let previousGames = []
+var varResult
+
 
 const powerball5 = {
     start: () => {
-        powerball5.initGame()
+        createGame()
+        showGame()
         setInterval(powerball5.main, 1000)
     },
     
     main: () => {
-        // console.log(`Time: ${tempTime}:${tempSec}`)
-        tempSec++
-        if(tempSec === 60){
-            tempTime++
-            tempSec = 0
-        }
+        const now = moment()
+        const minutes = now.minutes()
+        const seconds = now.seconds()
 
-        if ((tempTime + 1) % interval === 0 && tempSec === adjustedSec) {
-            powerball5.createGame()
-            powerball5.getResult()
-            console.log('miniGames', miniGames)
+        if(minutes % interval === 0 && seconds === 35) {
+            getResult()
         }
-
-        // powerball5.createGame()
-    },
-    initGame: () => {
-        var regTime = `Time: ${tempTime}:${tempSec}`
-        var gameTime = `Time: ${powerball5.getAheadTimeBaseOnInterval(tempTime)}:00`
-        gameInfo = {
-            round: powerball5.getAheadTimeBaseOnInterval(tempTime) / 5,
-            gameTime,
-            regTime,
-            resultTime: null,
+        if(minutes % interval === 0 && seconds === 50) {
+            if(varResult) {
+                showResult()
+            }
+            createGame()
+            showGame()
         }
-        miniGames.push(gameInfo)
-        console.log('miniGames', miniGames)
     },
 
     createGame: () => {
-        var regTime = `Time: ${tempTime}:${tempSec}`
-        var gameTime = `Time: ${powerball5.getAheadTimeBaseOnInterval(tempTime + 1)}:00`
-        gameInfo = {
-            round: powerball5.getAheadTimeBaseOnInterval(tempTime + 1) / 5,
-            gameTime,
-            regTime,
-            resultTime: null,
+        const now = moment()
+        const round = Math.floor(now.hours() * 60 + now.minutes()) / interval
+        if(!previousGames.some(g => g.round === round)) {
+            game = {
+                round: round,
+                gameDate: now.toISOString().split('T')[0],
+                result: [],
+                regDateTime: now.toISOString().replace('T', '').substring(0,19),
+                resultDateTime: null
+            }
         }
-        miniGames.push(gameInfo)
     },
 
+    showGame: () => {
+        timerCountDown = interval * 60
+        console.log(game)
+        console.log("Timer countdown:" + timerCountDown + "seconds")
+    },
     getResult: () => {
-        miniGames[0].resultTime = `Time: ${tempTime}:${tempSec}`
+
     },
+
+    // getResult: () => {
+    //     miniGames[0].resultTime = `Time: ${tempTime}:${tempSec}`
+    // },
     showResult: () => {
-        const currentTimeString = `Time: ${tempTime}:${tempSec}`;
-        miniGames.forEach(game => {
-            if (!game.resultTime) {
-                const gameTimeMoment = moment(game.gameTime, 'Time: HH:mm');
-                const currentTimeMoment = moment(currentTimeString, 'Time: HH:mm');
-    
-                if (currentTimeMoment.isSameOrAfter(gameTimeMoment)) {
-                    game.resultTime = currentTimeString;
-                    console.log(`Result Time Updated for Round ${game.round}: ${game.resultTime}`);
-                }
-            }
-        });
+        const now = moment()
+        game.result = varResult
+        game.resultDateTime = now.toISOString().replace('T', '').substring(0, 19)
+        previousGames.push(game)
+        console.log(game)
     },
+
 
 //   scrapeData: () => {
 //     tempTime++
@@ -97,23 +90,23 @@ const powerball5 = {
 //   },
   
 
-  generateRoundId: (minute) => {
-    var now = new Date()
-    var referenceTime = new Date(now)
-    referenceTime.setHours(0,0,0,0)
-    var intervalsElapsed = Math.floor((now - referenceTime) / (minute * 60 * 1000))
-    return intervalsElapsed + 1
-  },
-  getAheadTimeBaseOnInterval: (currentMin) => {
-    let timeIncrement = currentMin
-    while (true) {
-        timeIncrement++
-        if (timeIncrement % interval === 0) {
-            return timeIncrement
-        }
-    }
-}
-}
+//   generateRoundId: (minute) => {
+//     var now = new Date()
+//     var referenceTime = new Date(now)
+//     referenceTime.setHours(0,0,0,0)
+//     var intervalsElapsed = Math.floor((now - referenceTime) / (minute * 60 * 1000))
+//     return intervalsElapsed + 1
+//   },
+//   getAheadTimeBaseOnInterval: (currentMin) => {
+//     let timeIncrement = currentMin
+//     while (true) {
+//         timeIncrement++
+//         if (timeIncrement % interval === 0) {
+//             return timeIncrement
+//         }
+//     }
+// }
+// }
 // function generateRoundId(minute) {
 //   var now = new Date()
 
@@ -125,10 +118,10 @@ const powerball5 = {
 //   var roundId = intervalsElapsed + 1
 
 //   return roundId
-// }
-// setInterval(() => {
-//   console.log(generateRoundId(5))
-// }, 100000)
+}
+setInterval(() => {
+  console.log(generateRoundId(5))
+}, 100000)
 
 module.exports = powerball5
 

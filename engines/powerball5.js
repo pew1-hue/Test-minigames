9d958487a9340ed8b92e2ae54260632869f65e2b
pew1-hue <pblabitoria@gmail.com>
2024-01-26 11:48:06 +0900

    0.0.0

--- engines/powerball5.js
+++ engines/powerball5.js
@@ -1,54 +1,122 @@
 const moment = require('moment-timezone')
 const game = 'powerball5'
 const interval = 5
-let round = 0
+let tempTime = 4
+let tempSec = 30
+let miniGames = []
+let gameInfo = {}
+let adjustedSec = 35
+
 const powerball5 = {
-  start: () => {
-    setInterval(powerball5.scrapeData, 1000)
-  },
-  scrapeData: async () => {
-    try {
-      const now = moment();
-      const minutes = now.minutes();
-      const seconds = now.seconds();
-
-      const nextTargetMinute = round * interval
-
-      const setTime = now.format('HH:mm:ss')
+    start: () => {
+        powerball5.initGame()
+        setInterval(powerball5.main, 1000)
+    },
     
-      if (minutes === nextTargetMinute && seconds === 35) {
-        round += 1;
-        console.log(`${setTime} ${round} round`);
+    main: () => {
+        // console.log(`Time: ${tempTime}:${tempSec}`)
+        tempSec++
+        if(tempSec === 60){
+            tempTime++
+            tempSec = 0
+        }
+
+        if ((tempTime + 1) % interval === 0 && tempSec === adjustedSec) {
+            powerball5.createGame()
+            powerball5.getResult()
+            console.log('miniGames', miniGames)
+        }
+        // powerball5.createGame()
+    },
+    initGame: () => {
+        var regTime = `Time: ${tempTime}:${tempSec}`
+        var gameTime = `Time: ${powerball5.getAheadTimeBaseOnInterval(tempTime)}:00`
+        gameInfo = {
+            round: powerball5.getAheadTimeBaseOnInterval(tempTime) / 5,
+            gameTime,
+            regTime,
+            resultTime: null,
+        }
+        miniGames.push(gameInfo)
+        console.log('miniGames', miniGames)
+    },
+
+    createGame: () => {
+        var regTime = `Time: ${tempTime}:${tempSec}`
+        var gameTime = `Time: ${powerball5.getAheadTimeBaseOnInterval(tempTime + 1)}:00`
+        gameInfo = {
+            round: powerball5.getAheadTimeBaseOnInterval(tempTime + 1) / 5,
+            gameTime,
+            regTime,
+            resultTime: null,
+        }
+        miniGames.push(gameInfo)
+    },
+
+    getResult: () => {
+        miniGames[0].resultTime = `Time: ${tempTime}:${tempSec}`
+    },
+
+  scrapeData: () => {
+    tempTime++
+    try {
+      const now = moment()
+      const minutes = now.minutes()
+      const seconds = now.seconds()
+      const roundId = powerball5.generateRoundId(interval)
+
+      if ((minutes + 1) % interval === 0 && seconds === 35) {
+        const result = {
+          gameName: game,
+          date: now.format('YYYY-MM-DD'),
+          round: roundId,
+          gameDateTime: now.format('YYYY-MM-DDTHH:mm:ss.SSS') 
+        };
+
+        console.log(result)
       }
 
     } catch (err) {
       console.error(`An error occurred - ${game}:`, err)
-      console.log(`Trying to reconnect in 5 seconds... - ${game}`)
     }
-  }
+  },
+  
 
+  generateRoundId: (minute) => {
+    var now = new Date()
+    var referenceTime = new Date(now)
+    referenceTime.setHours(0,0,0,0)
+    var intervalsElapsed = Math.floor((now - referenceTime) / (minute * 60 * 1000))
+    return intervalsElapsed + 1
+  },
+  getAheadTimeBaseOnInterval: (currentMin) => {
+    let timeIncrement = currentMin
+    while (true) {
+        timeIncrement++
+        if (timeIncrement % interval === 0) {
+            return timeIncrement
+        }
+    }
 }
+}
+// function generateRoundId(minute) {
+//   var now = new Date()
+
+//   var referenceTime = new Date(now)
+//   referenceTime.setHours(0,0,0,0)
+
+//   var intervalsElapsed = Math.floor((now - referenceTime) / (minute * 60 * 1000))
+
+//   var roundId = intervalsElapsed + 1
+
+//   return roundId
+// }
+// setInterval(() => {
+//   console.log(generateRoundId(5))
+// }, 100000)
+
 module.exports = powerball5
 
 powerball5.start()
 
-// function updateGameRound() {
-//   let gameDateTime = moment()
-
-//   round +=1
-
-//   const localDateTime = gameDateTime.tz(moment.tz.guess()).format()
-
-
-//   console.log({
-//     gameName: game,
-//     date:  gameDateTime.format('YYYY-MM-DD'),
-//     round,
-//     gameDateTime: localDateTime
-//   })
-// }
-
-// updateGameRound()
-
-// setInterval(updateGameRound, 5 * 60000)
 
